ea05dc821503c09e92dad651ee8e2be7739a1ed3
sohamduck <duttasoham2222@gmail.com>
2023-12-14 00:26:43 +0530

    Removed Router to Deploy in GitHub Pages2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -45,7 +45,7 @@ export default function Navbar(props) {
       {/* < a className="navbar-brand" to="/">
         {props.title}
       </> */}
-      <a className="navbar-brand" href="/">
+      <a className="navbar-brand" href="#">
         {props.title}
       </a>
       <button
@@ -63,7 +63,7 @@ export default function Navbar(props) {
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item active">
-            <a className="nav-link" href="/">
+            <a className="nav-link" href="#">
               Home <span className="sr-only">(current)</span>
             </a>
           </li>
